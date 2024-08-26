a40203222f70443b4169d8db0b13925e08266b91
Jose Mena <163463282+TheFanking@users.noreply.github.com>
2024-08-26 19:20:58 -0400

    Actualizacion Main.java
    se actualiza el Main y se crea la función menú Principal que servirá para que el usuario interactúe con diferentes opciones.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,44 @@
-public class Main
-{
-  public static void main(String[] args)
-  {
-    System.out.println("Hello world!");
+import java.util.ArrayList;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class Main{
+
+  public static void main(String [] args) throws IOException
+  {  
+      Main app = new Main();
+      app.menuPrincipal();
   }
+  public void menuPrincipal() throws IOException
+  {
+      BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+      int opcion = -1;
+      while (opcion != 0)
+        {
+          
+          System.out.println("Menu Principal\n");
+          System.out.println("Selecciona una opcion");
+          System.out.println("1) Registrar Emprendedor");
+          System.out.println("2) Mostrar Informacion de un emprendedor");
+          System.out.println("0) Salir del programa");
+          String numero = lector.readLine();
+          opcion = Integer.parseInt(numero);
+          switch(opcion)
+            {
+                case 1:
+                  registrarEmprendedor();
+                  break;
+                case 2:
+                  mostrarInfoEmprendedor();
+                  break;
+                case 0:
+                  System.out.println("Saliendo del programa...");
+                  break;
+                default:
+                  System.out.println("Opcion incorrecta");
+                  System.out.println("Marque otra opcion");
+                  break;
+            }    
+        }  
+  }  
 }
