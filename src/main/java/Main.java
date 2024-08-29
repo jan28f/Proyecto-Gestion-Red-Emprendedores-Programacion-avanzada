b4ff88af671ba27314343e17123d57c742976d25
jan28f <hans.silva.g@mail.pucv.cl>
2024-08-29 14:38:28 -0400

    Se implemento submenus para modificar los valores de un emprendimiento

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,89 @@ public class Main
 		Main app = new Main();
 		app.menuPrincipal();
 	}
+
+	public void menuModificacionEmprendimiento(Emprendimiento emprendimiento)
+	{
+		Scanner scanner = new Scanner(System.in);
+		int opcion = -1;
+
+		do
+		{
+			System.out.println("¿Que desea hacer modificar?");
+			System.out.println("1) Propietario de la empresa");
+			System.out.println("2) Especialidad de la empresa");
+			System.out.println("3) Capital monetario de la empresa");
+			System.out.println("4) Empleados en la empresa");
+			System.out.println("5) Ningun cambio\n");
+			System.out.print("Ingrese una opcion: ");
+			opcion = scanner.nextInt();
+
+			switch (opcion)
+			{
+				case 1:
+					System.out.print("Ingrese el nombre del nuevo propietario: ");
+					scanner.nextLine();
+					String nuevoPropietario = scanner.nextLine();
+					emprendimiento.setNombrePropietario(nuevoPropietario);
+					System.out.print("\nSe ha cambiado el propietario");
+					break;
+				case 2:
+					System.out.print("Ingrese la nueva especialidad de la empresa: ");
+					scanner.nextLine();
+					String nuevaEspecialidad = scanner.nextLine();
+					emprendimiento.setEspecialidad(nuevaEspecialidad);
+					System.out.print("\nSe ha cambiado la especialidad de la empresa");
+					break;
+				case 3:
+					System.out.print("Ingrese el nuevo capital de la empresa: ");
+					int nuevoCapitalMonetario = scanner.nextInt();
+					emprendimiento.setCapitalMonetario(nuevoCapitalMonetario);
+					System.out.print("\nSe ha actualizado el capital de la empresa");
+					break;
+				case 4:
+					System.out.print("Ingrese la nueva cantidad de empleados: ");
+					int nuevoTotalEmpleados = scanner.nextInt();
+					emprendimiento.setTotalEmpleados(nuevoTotalEmpleados);
+					System.out.print("\nSe ha actualizado el total de empleados de la empresa");
+					break;
+				case 5:
+					break;
+				default:
+					System.out.println("Ingrese una opcion valida...");
+					break;
+			}
+		} while (opcion != 5);
+	}
+
+	public void menuEmprendimientos(Emprendimiento emprendimiento)
+	{
+		Scanner scanner = new Scanner(System.in);
+		int opcion = -1;
+
+		do
+		{
+			System.out.println("¿Que desea hacer?");
+			System.out.println("1) Ver informacion general de la empresa");
+			System.out.println("2) Modificar informacion");
+			System.out.println("3) Volver al menu principal\n");
+			System.out.print("Ingrese una opcion: ");
+			opcion = scanner.nextInt();
+
+			switch (opcion)
+			{
+				case 1:
+					break;
+				case 2:
+					menuModificacionEmprendimiento(emprendimiento);
+					break;
+				case 3:
+					break;
+				default:
+					System.out.println("Ingrese una opcion valida...");
+					break;
+			}
+		} while (opcion != 3);
+	}
 	
 	public void menuPrincipal() throws IOException
 	{
@@ -45,8 +128,8 @@ public class Main
 					Emprendimiento emprendimiento = emprendimientos.get(aBuscar);
 					if (emprendimiento != null)
 					{
-						String nombreEmprendimiento = emprendimiento.getNombreEmprendimiento();
-						System.out.println(nombreEmprendimiento);
+						System.out.println("Se ha encontrado un emprendimiento en el registro");
+						menuEmprendimientos(emprendimiento);
 					}
 					else
 						System.out.println("No se encontro " + aBuscar + " en el registro de emprendimientos");
@@ -87,7 +170,7 @@ public class Main
 					Emprendimiento nuevoEmprendimiento = new Emprendimiento(nombreEmprendimiento, nombreDueno, especialidadEmpresa, capitalMonetario, totalEmpleados);
 					
 					emprendimientos.put(nombreEmprendimiento, nuevoEmprendimiento);
-					System.out.print("Se ha registrado exitosamente su emprendimiento");
+					System.out.println("\nSe ha registrado exitosamente su emprendimiento");
 					break;
 				case 4:
 					break;
@@ -99,9 +182,9 @@ public class Main
 					System.out.println("Marque otra opcion");
 					break;
 		  	}
-			System.out.print("\nPresiona Enter para continuar...");
+			System.out.print("Presiona Enter para continuar...");
 			lector.readLine();    
 	  	} while (opcion != 5);
-	  	lector.close();
+		lector.close();
 	}
 }
