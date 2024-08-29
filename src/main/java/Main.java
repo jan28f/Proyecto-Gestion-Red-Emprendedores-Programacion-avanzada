d744669be84e6a01c1eb784280dd9845f6898cfb
jan28f <163349875+jan28f@users.noreply.github.com>
2024-08-29 02:09:12 +0000

    Modificacion de menu principal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,48 +1,54 @@
 import java.util.*;
+
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 public class Main{
 
-  public static void main(String [] args) throws IOException
-  {  
-    Main app = new Main();
-    app.menuPrincipal();
-  }
-  
-  public void menuPrincipal() throws IOException
+public static void main(String [] args) throws IOException
+{  
+  Main app = new Main();
+  app.menuPrincipal();
+}
+
+public void menuPrincipal() throws IOException
+{
+  HashMap<String, Emprendimiento> emprendimientos = new HashMap<String, Emprendimiento>();
+  HashMap<String, Proyecto> proyectos = new HashMap<String, Proyecto>();
+
+  BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+  int opcion = -1;
+  do
   {
-    HashMap<String, Emprendimiento> emprendimientos = new HashMap<String, Emprendimiento>();
-    HashMap<String, Proyecto> proyectos = new HashMap<String, Proyecto>();
-    
-      BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
-      int opcion = -1;
-      while (opcion != 0)
-        {
-          
-          System.out.println("Menu Principal\n");
-          System.out.println("Selecciona una opcion");
-          System.out.println("1) Registrar Emprendedor");
-          System.out.println("2) Mostrar Informacion de un emprendedor");
-          System.out.println("0) Salir del programa");
-          String numero = lector.readLine();
-          opcion = Integer.parseInt(numero);
-          switch(opcion)
-            {
-                case 1:
-                  registrarEmprendedor();
-                  break;
-                case 2:
-                  mostrarInfoEmprendedor();
-                  break;
-                case 0:
-                  System.out.println("Saliendo del programa...");
-                  break;
-                default:
-                  System.out.println("Opcion incorrecta");
-                  System.out.println("Marque otra opcion");
-                  break;
-            }    
-        }  
+    System.out.println("============================");
+    System.out.println("     Menu Principal\n");
+    System.out.println("============================");
+    System.out.println("1) Buscar emprendimiento");
+    System.out.println("2) Buscar proyecto");
+    System.out.println("3) Registrar emprendimiento");
+    System.out.println("4) Registrar proyecto");
+    System.out.println("5) Salir del programa");
+
+    String numero = lector.readLine();
+    opcion = Integer.parseInt(numero);
+    switch(opcion)
+    {
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        System.out.println("Saliendo del programa...");
+        break;
+      default:
+        System.out.println("Opcion invalida");
+        System.out.println("Marque otra opcion");
+        break;
+      }    
+    } while (opcion != 5);
   }  
 }
