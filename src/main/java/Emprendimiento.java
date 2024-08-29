be0d015aa061a1d43bfebb2f9b8cc260f27ae064
Jose Mena <163463282+TheFanking@users.noreply.github.com>
2024-08-29 18:48:36 -0400

    Update Emprendimiento.java
    creación de clase emprendimiento y concatenación con las clases apoyo y proyecto mediante la composicion

--- src/main/java/Emprendimiento.java
+++ src/main/java/Emprendimiento.java
@@ -72,4 +72,147 @@ public class Emprendimiento
     {
       return apoyos;
     }
+    public class Proyecto
+      {
+        private String nombreProyecto;
+        private String empresaEncargada;
+        private String liderProyecto;
+        private int personalRequerido;
+        private int personalActual;
+        private int costoEstimado;
+        private int gastoActual;
+        private String estadoActual;
+        
+        //Constructores
+        public Proyecto(String nombreProyecto, String empresaEncargada, String liderProyecto,int personalRequerido, int personalActual, int costoEstimado, int gastoActual,String estadoActual)
+        {
+           this.nombreProyecto = nombreProyecto;
+           this.empresaEncargada = empresaEncargada;
+           this.liderProyecto = liderProyecto;
+           this.personalRequerido = personalRequerido;
+           this.personalActual = personalActual;
+           this.costoEstimado = costoEstimado;
+           this.gastoActual = gastoActual;
+           this.estadoActual = estadoActual;
+        }
+        //Setters
+        public void setNombreProyecto(String nombreProyecto)
+        {
+          this.nombreProyecto = nombreProyecto;
+        }  
+        public void setEmpresaEncargada(String empresaEncargada)
+        {
+          this.empresaEncargada = empresaEncargada;
+        }
+        public void setLiderProyecto(String liderProyecto)
+        {
+          this.liderProyecto = liderProyecto;
+        }
+        public void setPersonalRequerido(int personalRequerido)
+        {
+          this.personalRequerido = personalRequerido;
+        }
+        public void setPersonalActual(int personalActual)
+        {
+          this.personalActual = personalActual;
+        }  
+        public void setCostoEstimado(int costoEstimado)
+        {
+          this.costoEstimado = costoEstimado;
+        }
+        public void setGastoActual(int gastoActual)
+        {
+          this.gastoActual = gastoActual;
+        }
+        public void setEstadoActual(String estadoActual)
+        {
+          this.estadoActual = estadoActual;
+        } 
+        //Gettters 
+        public String getNombreProyecto()
+        {
+          return nombreProyecto;
+        }
+        public String getEmpresaEncargada()
+        {
+          return empresaEncargada;
+        }
+        public String getLiderProyecto()
+        {
+          return liderProyecto;
+        }
+        public int getPersonalRequerido()
+        {
+          return personalRequerido;
+        } 
+        public int getPersonalActual()
+        {
+          return personalActual;
+        } 
+        public int getCostoEstimado()
+        {
+          return costoEstimado;
+        }
+        public int getGastoActual()
+        {
+          return gastoActual;
+        } 
+        public String getEstadoActual()
+        {
+          return estadoActual;
+        }  
+      }
+    public class Apoyo
+      {
+        private String nombreApoyo;
+        private int montoApoyo;
+        private String fechaVencimiento;
+        private String estadoApoyo;
+
+        //Constructor
+        public Apoyo(String nombreApoyo,int montoApoyo, String fechaVencimiento, String estadoApoyo)
+        {
+          this.nombreApoyo = nombreApoyo;
+          this.montoApoyo = montoApoyo;
+          this.fechaVencimiento = fechaVencimiento;
+          this.estadoApoyo = estadoApoyo;
+        }
+
+        //Setter
+        public void setNombreApoyo(String nombreApoyo)
+        {
+          this.nombreApoyo = nombreApoyo;
+          
+        }
+        public void setMontoApoyo(int montoApoyo)
+        {
+          this.montoApoyo = montoApoyo;
+        }  
+        public void setFechaVencimiento(String fechaVencimiento)
+        {
+          this.fechaVencimiento = fechaVencimiento;
+        }
+        public void setEstadoApoyo(String estadoApoyo)
+        {
+          this.estadoApoyo = estadoApoyo;
+        }  
+
+        //Gettters
+        public String getNombreApoyo()
+        {
+          return nombreApoyo;
+        }
+        public int getMontoApoyo()
+        {
+          return montoApoyo;
+        }
+        public String getFechaVencimiento()
+        {
+          return fechaVencimiento;
+        }
+        public String getEstadoApoyo()
+        {
+          return estadoApoyo;
+        }  
+      }  
   }
