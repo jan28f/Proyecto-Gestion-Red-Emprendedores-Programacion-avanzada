de9645c7b7ea8f3d9a28dd308776746d5eeeb0b8
Jose Mena <163463282+TheFanking@users.noreply.github.com>
2024-09-04 21:24:17 -0400

    Creacion RecursosApoyos.java
    monto de apoyo realizado

---
+++ src/main/java/RecursosApoyos.java
@@ -0,0 +1,35 @@
+public RecursosApoyos
+  {
+    private int totalEmpleados;
+    private int capital;
+    private int montoApoyo;
+    public RecursosApoyos(int totalEmpleados,int capital, int montoApoyo)
+      {
+        this.totalEmpleados = totalEmpleados;
+        this.capital = capital;
+        this.montoApoyo = montoApoyo;
+      }
+    //Gettters
+    public int getTotalEmpleados()
+      {
+        return totalEmpleados;
+      }
+    public int getCapital()
+      {
+        return capital;
+      }
+    public int getMontoApoyo()
+      {
+        return montoApoyo;
+      }
+    //Setters
+    public void setTotalEmpleados(int totalEmpleados)
+      {
+        this.totalEmpleados = totalEmpleados;
+      }
+
+    public void setMontoApoyo(int montoApoyo)
+      {
+        this.montoApoyo = montoApoyo;
+      }
+  }  
