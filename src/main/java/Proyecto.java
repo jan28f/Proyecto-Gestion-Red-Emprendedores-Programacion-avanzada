65fff4ab7a1839eaa067023d7749f67277381fcc
Jose Mena <163463282+TheFanking@users.noreply.github.com>
2024-09-04 21:38:38 -0400

    Create Proyecto.java

---
+++ src/main/java/Proyecto.java
@@ -0,0 +1,58 @@
+public class Proyecto
+  {
+    private String nombreProyecto;
+    private String encargado;
+    private int personalRequerido;
+    private int costoEstimado;
+    private String estadoActual;
+    public Proyecto(String nombreProyecto,String encargado,int personalRequerido, int costoEstimado, String estadoActual)
+    {
+      this.nombreProyecto = nombreProyecto;
+      this.encargado = encargado;
+      this.personalRequerido = personalRequerido;
+      this.costoEstimado = costoEstimado;
+      this.estadoActual = estadoActual;
+    }
+    //Getter
+    public String getNombreProyecto()
+    {
+      return nombreProyecto;
+    } 
+    public String getEncargado()
+    {
+      return encargado;
+    }
+    public int getPersonalRequerido()
+    {
+      return personalRequerido;
+    }
+    public int getCostoEstimado()
+    {
+      return costoEstimado;
+    }
+    public int getEstadoActual()
+    {
+      return estadoActual;
+    }
+    //Setter
+    public void setNombreProyecto(String nombreProyecto)
+    {
+      this.nombreProyecto = nombreProyecto;
+    } 
+    public void setEncargado(String encargado)
+    {
+      this.encargado = encargado;
+    }
+    public void setPersonalRequerido(int personalRequerido)
+    {
+      this.personalRequerido = personalRequerido;
+    }
+    public void setCostoEstimado(int costoEstimado)
+    {
+      this.costoEstimado = costoEstimado;
+    }
+    public void setEstadoActual(String estadoActual)
+    {
+      this.estadoActual = estadoActual;
+    }  
+  }  
